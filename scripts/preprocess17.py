818d278ab108a6f020ad693a581914fed243da78
NamSor <elian.carsenat@namsor.com>
2025-02-01 18:00:21 +0100

    added dataset17

---
+++ scripts/preprocess17.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import myconfig
+
+# preprocessing script for the Espace Richaud data
+file_path = myconfig.root_path+"17_Dicier/"
+file_name = file_path+"Epicerie Versailles hackaton 2025_ELC.xlsx"
+# make a dictionary that maps the month name in french to a number
+french_to_eng = {
+    'janv.':'1',
+    'févr.':'2',
+    'mars':'3',
+    'avr.':'4',
+    'mai':'5',
+    'juin':'6',
+    'juil.':'7',
+    'août':'8',
+    'sept.':'9',
+    'oct.':'10',
+    'nov.':'11',
+    'déc.':'12'}
+
+# Load the Excel file
+excel_file = pd.ExcelFile(file_name)
+
+# Initialize an empty DataFrame to hold all the data
+all_data = pd.DataFrame()
+
+total_2024 = 0
+
+make_graph = True
+
+# Iterate through each sheet in the Excel file
+for sheet_name in excel_file.sheet_names:
+    # Read the data from the current sheet
+    sheet_data = pd.read_excel(file_name, sheet_name=sheet_name)
+    # filter out the rows with missing values in the DATES column
+    sheet_data = sheet_data.dropna(subset=['Date'])
+    # remove the trailing .0 in date 2024.0
+    sheet_data['year'] = sheet_data['Date'].astype(str).str.replace('.0', '')
+    sheet_data['month'] = sheet_data['Mois'].map(french_to_eng)
+    sheet_data['day'] = sheet_data['Jour'].astype(str).str.replace('.0', '')
+    print(sheet_data)
+    sheet_data['date_fr'] = sheet_data['day'] + '/' + sheet_data['month'] + '/' + sheet_data['year']
+    print(sheet_data)
+    # parse French dates in format 28/12/2024 to a standard date format 2024-12-28
+    sheet_data['date'] = pd.to_datetime(sheet_data['date_fr'], format='%d/%m/%Y')
+
+    # Append the data to the all_data DataFrame
+    all_data = pd.concat([all_data, sheet_data], axis=0)
+
+# only keep date and CA HT columns
+all_data = all_data[['date', 'CA HT']]
+print(all_data)
+
+# sum the number of visitors for the year 2024
+total_2024 = all_data[all_data[myconfig.field_date].dt.year == 2024]['CA HT'].sum()
+
+print("total_2024: ", total_2024)
+all_data['versailles_epicerie1_ca_ttc_base_100k'] = all_data['CA HT'] / total_2024 * 100000
+# sort by date
+all_data = all_data.sort_values(myconfig.field_date, ascending=True)
+# group by date and sum the number of transactions and the total revenue
+# if make_graph: then make a graph with the total revenue per day and the number of transations per day and save it to a file
+if make_graph:
+    plt.figure(figsize=(10, 6))
+    plt.plot(all_data[myconfig.field_date], all_data['versailles_epicerie1_ca_ttc_base_100k'], marker='o', linestyle='-', color='red', label='Total Revenue')
+    plt.title('Total Revenue Per Day')
+    plt.xlabel('Date')
+    plt.ylabel('Revenue')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.legend(title='Revenue')
+    plt.tight_layout()
+    plt.savefig(file_path+"versailles_epicerie_1.png")
+    plt.close()
+
+
+# all_data = all_data.rename(columns={'date_standard': myconfig.field_date})
+# rename the column "affluence" to "ancienne_poste_affluence_base100k"
+all_data = all_data.rename(columns={'versailles_epicerie1_ca_ttc_base_100k': myconfig.field_y})
+
+# add a column "y_value" with the value 
+all_data[myconfig.field_seriesname]='versailles_epicerie1_ca_ttc_base_100k'
+# add a column train_valid_test
+all_data[myconfig.field_train_valid_test] = 'train'
+# set 'train_valid_test' to 'valid' for dates between 2024-12-01 and 2024-12-14 inclusive
+all_data.loc[(all_data[myconfig.field_date] >= myconfig.date_split_train) & (all_data[myconfig.field_date] < myconfig.date_split_valid), myconfig.field_train_valid_test] = 'valid'
+# set 'train_valid_test' to 'test' for dates 2024-12-15 and after
+all_data.loc[all_data[myconfig.field_date] >= myconfig.date_split_valid, myconfig.field_train_valid_test] = 'test'
+# drop all columns except "date_standard" and "affluence"
+all_data = all_data[[myconfig.field_date, myconfig.field_y, myconfig.field_seriesname, myconfig.field_train_valid_test]]
+# remove all data after the test date
+all_data = all_data[all_data[myconfig.field_date] <= myconfig.date_split_test]
+# sort by date
+all_data = all_data.sort_values(myconfig.field_date, ascending=True)
+# save the DataFrame to a CSV file
+all_data.to_csv(myconfig.opendata_path+'versailles_epicerie_1.csv', index=False)
+
+all_train_valid_data = all_data[all_data[myconfig.field_train_valid_test] != 'test']
+all_train_valid_data.to_csv(myconfig.git_path+'versailles_epicerie_1.csv', index=False)
+
+# Display the combined DataFrame
+print(all_data)
