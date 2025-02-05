60994f4de6d0ae6faa96876fb6c34e493e64b69f
NamSor <elian.carsenat@namsor.com>
2025-02-05 12:15:24 +0100

    try a different meteo dataset

--- scripts/preprocess80_meteo.py
+++ scripts/preprocess80_meteo.py
@@ -5,37 +5,43 @@ import os
 
 # preprocessing script for the Gonzague data
 # Define the path to the Excel file
-file_path = myconfig.extra_path+"MeteoFrance/"
-file = "donnees-synop-essentielles-omm.csv"
-# read the CSV file sample.txt
-all_data = pd.read_csv(file_path+file, sep=';')
+file_path = myconfig.extra_path+"MeteoFrance/manual_downloads/"
 
-print(all_data.head())  
+# read from the filesystem the list of all files in the directory file_path
+files = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f)) and f.endswith('.csv')]
 
-# only keep the columns "Date" and "Température (°C)" "Précipitations dans les 24 dernières heures" "Vitesse du vent moyen 10 mn (m/s)" "Nebulosité totale"
-all_data = all_data[['Date', 'Température (°C)', 'Précipitations dans les 24 dernières heures', 'Vitesse du vent moyen 10 mn', 'Nebulosité totale', 'region (name)', 'department (code)']]
-# filter out the rows with missing values in the region or department column
-all_data = all_data.dropna(subset=['region (name)', 'department (code)'])
-# filter out the rows with missing values in the temperature column
-all_data = all_data.dropna(subset=['Température (°C)', 'Vitesse du vent moyen 10 mn'])
-# set precipitation to 0 if missing
-all_data['Précipitations dans les 24 dernières heures'] = all_data['Précipitations dans les 24 dernières heures'].fillna(0)
-# set Nebulosité totale to 0 if missing
-all_data['Nebulosité totale'] = all_data['Nebulosité totale'].fillna(0)
-# find the columns with missing values or with non numeric values
-print(all_data.isnull().sum())
+all_data = pd.DataFrame()
+# iterate over the files in the directory
+for file in files:
+    # the department number is in the filename Q_75_latest-2024-2025_RR-T-Vent.csv
+    # extract the department number from the filename
+    department_code = file.split('_')[1]
+    # read the CSV file
+    file_data = pd.read_csv(file_path+file, sep=';')
+    # check only keep the simplest columns
+    file_data = file_data[['NUM_POSTE', 'NOM_USUEL', 'AAAAMMJJ', 'RR', 'TM', 'FFM']]
+    
+    # oarse the data in the column "AAAAMMJJ" to a standard date format "YYYY-MM-DD"
+    file_data[myconfig.field_date] = pd.to_datetime(file_data['AAAAMMJJ'], format='%Y%m%d')
+    # filter all rows with missing values in the column "RR" or "TM" or "FFM"
+    file_data = file_data.dropna(subset=['RR', 'TM', 'FFM'])
+    file_data['department_code'] = department_code
+    print(file_data.head())  
+    # Append the data to the all_data DataFrame
+    all_data = pd.concat([all_data, file_data], axis=0)
 
+all_data = all_data[[myconfig.field_date, 'department_code', 'RR', 'TM', 'FFM']]
+# rename columns RR to RR_precipitaton, TM to TM_temperature, FFM to FFM_vent
+all_data = all_data.rename(columns={'RR': 'RR_precipitation', 'TM': 'TM_temperature', 'FFM': 'FFM_vent'})
 
-# create a date_std column with the date in the format "2024-12-24" by truncating the time part from Date as string
-all_data['date_std'] = all_data['Date'].str.slice(0, 10)
-all_data[myconfig.field_date] = pd.to_datetime(all_data['date_std'])
-# drop the column date_std
-all_data = all_data.drop(columns=['date_std', 'Date'])
-# average the data by date, region and department
-all_data = all_data.groupby([myconfig.field_date, "region (name)", "department (code)"]).mean().reset_index()
+all_data = all_data.groupby([myconfig.field_date, 'department_code']).mean().reset_index()
+
 print(all_data.head())
 # keep the meteo till 2024-12-31
 all_data = all_data[all_data[myconfig.field_date] <= myconfig.date_split_test]
 all_data.to_csv(myconfig.git_extra_path+'france_meteo.csv', index=False)
 
 
+
+
+    
