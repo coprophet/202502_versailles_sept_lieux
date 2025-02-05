b983be1ffe741b2349d706842d1df2894ea6b66a
NamSor <elian.carsenat@namsor.com>
2025-02-05 15:58:08 +0100

    add fr holidays

---
+++ scripts/preprocess80_joursférié.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import myconfig
+import os
+
+# preprocessing script for the Gonzague data
+# Define the path to the Excel file
+file_path = myconfig.extra_path+"JoursFériésFrance/"
+file_name = "jours_feries_metropole.csv"
+
+all_data = pd.read_csv(file_path+file_name,delimiter=',')
+all_data['ToDateTime'] = pd.to_datetime(all_data['date'], format='%Y-%m-%d', utc=True)
+all_data['ToDate'] = all_data['ToDateTime'].dt.date
+all_data['ToDateString'] = all_data['ToDateTime'].dt.strftime('%Y-%m-%d')
+all_data['jour_ferie'] = 1
+print(all_data.head())
+# drop all colums except ToDateString and IsHoliday
+all_data[myconfig.field_date] = pd.to_datetime(all_data['ToDateString'])
+all_data.drop(columns={'ToDateString','annee','zone','nom_jour_ferie','ToDateTime','ToDate'},inplace=True)
+
+print(all_data.head())
+
+all_data = all_data[all_data[myconfig.field_date] <= myconfig.date_split_test]
+# reorder columns 
+all_data.to_csv(myconfig.git_extra_path+'jours_ferie.csv', index=False)
